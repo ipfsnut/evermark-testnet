4fae15aae0e307e0d88940ade9c21693d34c4ad5
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2025-05-27 02:27:35 -0500

    try without screenshoturls

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
     target: 'es2020',
-    chunkSizeWarningLimit: 2000, // Just suppress warnings
+    chunkSizeWarningLimit: 2000,
   },
   esbuild: {
     target: 'es2020',
